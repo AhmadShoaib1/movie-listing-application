6b445e21a1a950aa933757125cca6bf7c9a16102
AhmadShoaib1 <nabias1@student.op.ac.nz>
2025-04-06 11:09:11 +1200

    intial commit

---
+++ movie-listing-application/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <div className="p-6">
+        <h1 className="text-2xl font-bold">🎬 Movie Listings Starter Ready!</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
