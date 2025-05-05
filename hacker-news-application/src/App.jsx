0c1dcb6d762312d526f66c59b40fe0e44100cc94
Ahmad Nabizada (1000113782) <NABIAS1@student.op.ac.nz>
2025-05-06 09:30:30 +1200

    basic structure for LeaderSearch and routing in app.jsx, the structure is given bcs it was giving errors for soem reason

--- hacker-news-application/src/App.jsx
+++ hacker-news-application/src/App.jsx
@@ -5,16 +5,25 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/navbar'
 import StoryList from './components/StoryList';
+import StoryDetails from './components/StoryDetail';
 
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <Router>
-      <Navbar/>
+      <Navbar />
+      <Routes>
+        <Route path="/ask-stories" element={<StoryList category="askstories" />} />
+        <Route path="/best-stories" element={<StoryList category="beststories" />} />
+        <Route path="/job-stories" element={<StoryList category="jobstories" />} />
+        <Route path="/new-stories" element={<StoryList category="newstories" />} />
+        <Route path="/show-stories" element={<StoryList category="showstories" />} />
+        <Route path="/top-stories" element={<StoryList category="topstories" />} />
+        <Route path="/leaders" element={<LeaderSearch />} />
+        <Route path="/story/:id" element={<StoryDetails />} /> 
+      </Routes>
     </Router>
-  )
+  );
 }
 
 export default App
---
+++ hacker-news-application/src/components/LeaderSearch.jsx
@@ -0,0 +1,11 @@
+function LeaderSearch() {
+    return (
+      <div>
+        <h1>Leader Search</h1>
+        {/* You can add leader search functionality here */}
+      </div>
+    );
+  }
+  
+  export default LeaderSearch;
+  
