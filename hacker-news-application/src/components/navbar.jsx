8cd37e26aae474e6617082c540a00eed09f3f2dd
Ahmad Nabizada (1000113782) <NABIAS1@student.op.ac.nz>
2025-05-06 09:00:12 +1200

    feat: finished navbar

--- hacker-news-application/src/components/navbar.jsx
+++ hacker-news-application/src/components/navbar.jsx
@@ -8,6 +8,11 @@ function Navbar() {
       <ul className="flex justify-around">
         <li><Link to="/ask-stories">Ask Stories</Link></li>
         <li><Link to="/best-stories">Best Stories</Link></li>
+        <li><Link to="/job-stories">Job Stories</Link></li>
+        <li><Link to="/new-stories">New Stories</Link></li>
+        <li><Link to="/show-stories">Show Stories</Link></li>
+        <li><Link to="/top-stories">Top Stories</Link></li>
+        <li><Link to="/leaders">Leaders</Link></li>
       </ul>
     </nav>
   );
