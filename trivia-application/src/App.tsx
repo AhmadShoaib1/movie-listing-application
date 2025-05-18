7113e10024cca2375d962a6488e4f837d425d024
AhmadShoaib1 <nabias1@student.op.ac.nz>
2025-05-18 13:36:46 +1200

    idk managed to fetch question but tailwind and css always give issue, ignore the delted files

--- trivia-application/src/App.tsx
+++ trivia-application/src/App.tsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from "react";
+
+const fetchTriviaQuestions = async () => {
+  const response = await fetch("https://opentdb.com/api.php?amount=10");
+  const data = await response.json();
+
+  if (data.response_code !== 0) {
+    throw new Error("Failed to fetch trivia questions.");
+  }
+
+  console.log(data.results);
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  useEffect(() => {
+    fetchTriviaQuestions();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>Trivia App</h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
