acacb72cddce720090329e530c6412e3d916cb0e
AhmadShoaib1 <nabias1@student.op.ac.nz>
2025-06-08 17:21:35 +1200

    feat: error handling in case of the task is empty

--- Emerging-technologies-application/src/components/todoinput.jsx
+++ Emerging-technologies-application/src/components/todoinput.jsx
@@ -2,15 +2,20 @@ import { createSignal } from 'solid-js';
 
 function TodoInput(props) {
   const [input, setInput] = createSignal('');
+  const [error, setError] = createSignal('');
 
   const handleAdd = () => {
     const trimmed = input().trim();
-    if (!trimmed) return;
+    if (!trimmed){
+        setError('Task cannot be empty');
+        return;
+    }
     props.onAdd(trimmed);
     setInput('');
   };
 
   return (
+    <div class="mb-4">
     <div class="flex gap-2 mb-4">
       <input
         class="border p-2 flex-1"
@@ -21,6 +26,8 @@ function TodoInput(props) {
       <button class="bg-blue-600 text-white px-4 py-2" onClick={handleAdd}>
         Add
       </button>
+    </div>
+    {error() && (<p class="text-red-600">{error()}</p>)}
     </div>
   );
 }
